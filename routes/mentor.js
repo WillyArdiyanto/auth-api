cc4799e86ec97dfc8b7b1678fe0e3420ef220eff
WillyArdiyanto <willyardiyanto2@gmail.com>
2022-05-23 22:40:35 +0700

    add CRUD

--- routes/mentor.js
+++ routes/mentor.js
@@ -17,6 +17,7 @@ mentor_route.post("/", async (req, res) => {
   }
 });
 
+//get all mentors
 mentor_route.get("/", async (req, res) => {
   try {
     const mentors = await Mentor.find();
@@ -27,4 +28,44 @@ mentor_route.get("/", async (req, res) => {
   }
 });
 
+
+//get specific mentor
+mentor_route.get("/:mentorId", async (req, res) => {
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId);
+    res.json(mentor);
+  } catch (err) {
+    res.status(400).send(err);
+    console.log(err);
+  }
+});
+
+//delete mentor
+mentor_route.delete("/:mentorId", async (req, res) => {
+  try {
+    const removeMentor = await Mentor.remove({_id: req.params.mentorId});
+    res.json(removeMentor);
+  } catch (err) {
+    res.status(400).send(err);
+    console.log(err);
+  }
+});
+
+//Update mentor
+mentor_route.delete("/:mentorId", async (req, res) => {
+  try {
+    const updateMentor = await Mentor.updateOne(
+      {_id: req.params.mentorId},
+      {$set: {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        expertise: req.body.expertise
+      }});
+    res.json(updateMentor);
+  } catch (err) {
+    res.status(400).send(err);
+    console.log(err);
+  }
+});
 module.exports = mentor_route;
