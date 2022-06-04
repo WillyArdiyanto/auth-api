aaf18fa31d078386583a744fcea8177691d419f7
WillyArdiyanto <willyardiyanto2@gmail.com>
2022-06-05 06:46:04 +0700

    add validator prototype

--- routes/mentor.js
+++ routes/mentor.js
@@ -3,12 +3,16 @@ const mentor_route = require("express").Router();
 //import controller
 const mentorController = require('../controller/mentor');
 
+//import helpers
+const { validateParam, schemas } = require('../helpers/routeHelper');
+
 mentor_route.route("/")
   .post(mentorController.addMentor)
   .get(mentorController.getAllMentor)
 
 mentor_route.route("/:mentorId")
-  .get(mentorController.getMentorById)
+  // .get((validateParam(schemas.idSchema, 'mentorId')), mentorController.getMentorById)//with validator
+  .get(mentorController.getMentorById)//without validator
   .delete(mentorController.deleteMentorById)
   .patch(mentorController.updateMentorById)
 
